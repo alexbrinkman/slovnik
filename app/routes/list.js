979e42070cf0f839dd8cce0c429b926df692b1c1
Alex Brinkman <alex@enlightenedpixel.com>
2015-01-28 20:00:38 -0700

    Updates list route temporary debugging.

--- app/routes/list.js
+++ app/routes/list.js
@@ -1,10 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function (params) {
-    // return this.store.find('list', params.list_id);
-    return this.store.find('list', 'qatnn');
-  },
   setupController: function(controller, model) {
     controller.set('model', model);
   }
